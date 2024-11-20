1c0252692c8121bc30d3437c94398069807dfcfc
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-21 02:04:15 +0530

    Change door creation route to '/create' for clarity

--- backend/routes/doorRoutes.js
+++ backend/routes/doorRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const doorController = require('../controllers/doorController');
 
 // Route to create a new door
-router.post('/', doorController.createDoor);
+router.post('/create', doorController.createDoor);
 
 // Route to get a door by ID
 router.get('/:id', doorController.getDoorById);
