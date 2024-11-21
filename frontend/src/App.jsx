e195d40ae951b949be5ea66f0d56ccf5b81ea6bf
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-21 10:01:55 +0530

    Add routes for DoorDetails and QRGenerator, and include NotFound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Doors from './pages/Doors';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import UserProfile from './pages/UserProfile';
 import Users from './pages/Users';
+import UserProfile from './pages/UserProfile';
+import Doors from './pages/Doors';
+import DoorDetails from './pages/DoorDetails';
+import Settings from './pages/Settings';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import QRGenerator from './pages/QRGenerator';
 
 const App = () => {
   return (
@@ -15,8 +18,11 @@ const App = () => {
         <Route path="/users" element={<Users />} />
         <Route path="/users/:id" element={<UserProfile />} />
         <Route path="/doors" element={<Doors />} />
+        <Route path="/doors/:id" element={<DoorDetails />} />
+        <Route path="/create-door" element={<QRGenerator />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
