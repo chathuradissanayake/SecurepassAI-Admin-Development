94297363a6ef66aef0dc3d2f24e17e9dad9c2b4c
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-22 11:24:57 +0530

    Update Door model to require location field and fix model name

--- backend/models/Door.js
+++ backend/models/Door.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 const doorSchema = new Schema({
   doorCode: { type: String, required: true, unique: true },
   doorName: { type: String, required: true },
-  location: { type: String },
-  approvedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }] // New field for approved users
+  location: { type: String, required: true },
+  approvedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 }, { timestamps: true });
 
-const Door = mongoose.model("Doors", doorSchema);
+const Door = mongoose.model('Door', doorSchema); 
 
 module.exports = Door;
