2b0aed4b168004b50d5ae9f144e3abb5f11d82c7
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-12-06 02:44:28 +0530

    The pending requests list on dashbord sorted as the time

--- frontend/src/components/PendingRequests.jsx
+++ frontend/src/components/PendingRequests.jsx
@@ -7,8 +7,10 @@ const PendingRequests = () => {
   useEffect(() => {
     const fetchPendingRequests = async () => {
       try {
-        const response = await axios.get('/api/permission-requests/pending-requests'); // Adjust the endpoint based on your backend setup
-        setRequests(response.data);
+        const response = await axios.get('/api/permission-requests/pending-requests');
+        // Sort the requests by requestTime in descending order
+        const sortedRequests = response.data.sort((a, b) => new Date(b.requestTime) - new Date(a.requestTime));
+        setRequests(sortedRequests);
       } catch (error) {
         console.error('Error fetching pending requests:', error);
       }
@@ -21,11 +23,10 @@ const PendingRequests = () => {
     <div>
       {requests.length > 0 ? (
         <ul>
-        
           {requests.map((request) => (
             <div
               key={request._id}
-              className=" items-center mb-4 bg-gray-100 p-3 rounded-lg"
+              className="items-center mb-4 bg-gray-100 p-3 rounded-lg"
             >
               <h2 className="text-lg font-semibold text-gray-700">
                 {request.name}
@@ -33,7 +34,6 @@ const PendingRequests = () => {
               <p>
                 <strong>Location:</strong> {request.roomName}
               </p>
-              
               <p>
                 <strong>Message:</strong> {request.message}
               </p>
