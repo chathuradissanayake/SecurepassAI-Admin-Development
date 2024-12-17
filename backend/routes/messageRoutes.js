61a46813fff7179a72c4b86241bb068f93f072e8
IT20272654 <shavindurajapaksha@gmail.com>
2024-12-17 23:45:50 +0530

    Updated Message Routes

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,12 +1,13 @@
 // routes/messageRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getMessages, toggleReadState} = require('../controllers/messageController');
+const { getMessages, toggleReadState, updateMessageDetails} = require('../controllers/messageController');
 
 // Route to get all messages
 router.get("/messages", getMessages);
 
 // Route to mark a message as read
 router.patch("/messages/:id/toggle-read", toggleReadState);
+router.patch("/messages/:id/update", updateMessageDetails);
 
 module.exports = router;
