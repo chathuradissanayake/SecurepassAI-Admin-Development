d4e9c90e308bb1b96bbf93b05546c13be9667af3
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-12-18 16:58:14 +0530

    Add Company model with schema for company details and admin references

---
+++ backend/models/Company.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const companySchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  admins: [{ type: Schema.Types.ObjectId, ref: 'AdminUser' }],
+}, { timestamps: true });
+
+const Company = mongoose.model('Company', companySchema);
+
+module.exports = Company;
