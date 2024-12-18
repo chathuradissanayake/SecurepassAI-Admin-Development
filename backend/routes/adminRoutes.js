a877baf334392e73cebcc420eef4110b66ad3e25
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-12-18 16:58:17 +0530

    Add routes for creating companies and admin users in adminRoutes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { registerAdminUser, loginAdminUser, getCurrentAdminUser } = require('../controllers/adminAuthController');
+const { createCompany, createAdminUser } = require('../controllers/superAdminController');
 const { authMiddleware, roleMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -12,9 +13,10 @@ router.post('/login', loginAdminUser);
 // Get current admin user
 router.get('/me', authMiddleware, getCurrentAdminUser);
 
-// Protected route for SuperAdmin
-router.post('/create-company', authMiddleware, roleMiddleware(['SuperAdmin']), (req, res) => {
-  
-});
+// Create a new company (SuperAdmin only)
+router.post('/create-company', authMiddleware, roleMiddleware(['SuperAdmin']), createCompany);
+
+// Create a new admin user (SuperAdmin only)
+router.post('/create-admin', authMiddleware, roleMiddleware(['SuperAdmin']), createAdminUser);
 
 module.exports = router;
