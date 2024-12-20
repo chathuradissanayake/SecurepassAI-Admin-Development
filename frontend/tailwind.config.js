c6a7e7c94ce9c6c7f3d0eaf0518e55767b71fda5
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-12-20 23:18:42 +0530

    Developed the Dark theme for Admin App

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Use 'media' for system preference
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
