a85d5f99ac3bf1c44cdf7a213be684f701561951
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-12-23 16:42:06 +0530

    Refactor message schema to use ObjectId for user and company references, add company field, and set default createdAt timestamp

--- backend/models/Messages.js
+++ backend/models/Messages.js
@@ -1,9 +1,9 @@
-// models/messages.js
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   userId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   },
   message: {
@@ -12,16 +12,21 @@ const messageSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
+    default: Date.now,
     required: true
   },
   status: { 
     type: String, 
     enum: ["unread", "read"], 
-    default: "unread" },
-
-
+    default: "unread" 
+  },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company',
+    required: true
+  }
 });
 
-const Message = mongoose.model('contactus', messageSchema);
+const Message = mongoose.model('Message', messageSchema);
 
 module.exports = Message;
