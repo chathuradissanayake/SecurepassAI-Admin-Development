75b20f9e2df2298030bfd1e82a8b7a6393505e43
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-12-23 21:52:41 +0530

    Repo Updated

--- frontend/src/pages/Companies.jsx
+++ frontend/src/pages/Companies.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 
