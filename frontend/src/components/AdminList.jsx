8ccd650222ba3e9176e6e3e8708388285bc115db
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-12-25 00:53:20 +0530

    Creating admin page redesigned as a popup model

--- frontend/src/components/AdminList.jsx
+++ frontend/src/components/AdminList.jsx
@@ -1,10 +1,76 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { default as React, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Modal from '../components/Modal';
 
 const AdminList = () => {
   const [adminUsers, setAdminUsers] = useState([]);
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [companyId, setCompanyId] = useState('');
+  const [companies, setCompanies] = useState([]);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response1 = await axios.get(`${import.meta.env.VITE_API_URL}/api/admin/companies`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setCompanies(response1.data);
+      } catch (err) {
+        console.error('Failed to fetch companies', err);
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/admin/create-admin`, {
+        firstName,
+        lastName,
+        email,
+        password,
+        companyId,
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setSuccess('Admin user created successfully');
+      setError('');
+
+      
+
+      // Copy login credentials to clipboard
+      const credentials = `Email: ${email}\nPassword: ${password}`;
+      await navigator.clipboard.writeText(credentials);
+      alert('Login credentials copied to clipboard. Please store them in a safe place and share them with the new admin user.');
+
+      // Optionally, navigate to another page or reset the form
+      setIsModalVisible(false);
+      
+    } catch (err) {
+      setError('Failed to create admin user');
+      setSuccess('');
+    }
+
+    
+  };
 
   useEffect(() => {
     const fetchAdminUsers = async () => {
@@ -26,6 +92,8 @@ const AdminList = () => {
     fetchAdminUsers();
   }, []);
 
+
+
   return (
     <div className="p-4 border dark:border-none rounded-lg shadow-sm bg-white dark:bg-slate-600">
 
@@ -33,7 +101,7 @@ const AdminList = () => {
       <div className="flex justify-between items-center mb-4">
       <h2 className="text-xl font-semibold dark:text-slate-100 mb-4">Admin List</h2>
             <button
-              onClick={() => navigate('/create-admin')}
+              onClick={() => setIsModalVisible(true)}
               className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
             >
               Create Admin
@@ -89,6 +157,74 @@ const AdminList = () => {
               </tbody>
             </table>
           </div>
+
+
+          <Modal isVisible={isModalVisible} onClose={() => setIsModalVisible(false)}>
+              <h2 className="text-xl font-semibold text-gray-800 dark:text-white">Create Admin User</h2>
+              {error && <p className="text-red-500">{error}</p>}
+              {success && <p className="text-green-500">{success}</p>}
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                  <label className="block text-slate-700 dark:text-slate-200">First Name</label>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-slate-700 dark:text-slate-200">Last Name</label>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-slate-700 dark:text-slate-200">Email</label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-slate-700 dark:text-slate-200">Password</label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-slate-700 dark:text-slate-200">Company</label>
+                  <select
+                    value={companyId}
+                    onChange={(e) => setCompanyId(e.target.value)}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                    required
+                  >
+                    <option value="">Select a company</option>
+                    {companies.map((company) => (
+                      <option key={company._id} value={company._id}>
+                        {company.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                  Create Admin
+                </button>
+              </form>
+          </Modal>
         </div>
       
     
