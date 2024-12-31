ebd96a0edc25942e57d931016ac0f8e8462804b5
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-12-31 23:44:46 +0530

    Email validation added when updating user information
    toastify added for server response

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,15 +1,19 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Header from '../components/Header';
-import Modal from '../components/Modal';
-import Sidebar from '../components/Sidebar';
-import Spinner from '../components/Spinner';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
 import UPDoorAccess from "../components/UPDoorAccess";
 import UPHistory from "../components/UPHistory";
 import UPPermissionRequests from "../components/UPPermissionRequests";
 import avatar from "../assets/avatar.png";
-import ConfirmationModal from '../components/ConfirmationModal'; // Import the ConfirmationModal component
+import ConfirmationModal from "../components/ConfirmationModal";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import { BiError } from "react-icons/bi";
 
 const UserProfile = () => {
   const { id } = useParams();
@@ -22,16 +26,18 @@ const UserProfile = () => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    userId: ''
+    firstName: "",
+    lastName: "",
+    email: "",
+    userId: "",
   });
+  const [emailUnique, setEmailUnique] = useState(null);
+  const [emailError, setEmailError] = useState("");
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         const response = await axios.get(`/api/users/${id}`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -43,12 +49,12 @@ const UserProfile = () => {
           firstName: response.data.firstName,
           lastName: response.data.lastName,
           email: response.data.email,
-          userId: response.data.userId
+          userId: response.data.userId,
         });
         setPendingRequests(response.data.pendingRequests);
         setLoading(false);
       } catch (err) {
-        console.error('Error fetching user:', err);
+        console.error("Error fetching user:", err);
         setError(err.message);
         setLoading(false);
       }
@@ -56,7 +62,7 @@ const UserProfile = () => {
 
     const fetchHistory = async () => {
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         const response = await axios.get(`/api/users/${id}/history`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -65,7 +71,7 @@ const UserProfile = () => {
         });
         setHistoryRecords(response.data);
       } catch (err) {
-        console.error('Error fetching history:', err);
+        console.error("Error fetching history:", err);
       }
     };
 
@@ -79,7 +85,7 @@ const UserProfile = () => {
 
   const handleSave = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       await axios.put(`/api/users/${id}`, formData, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -88,8 +94,9 @@ const UserProfile = () => {
       });
       setUser(formData);
       setIsEditModalOpen(false);
+      toast.success("User information updated successfully!");
     } catch (err) {
-      console.error('Error updating user:', err);
+      console.error("Error updating user:", err);
       setError(err.message);
     }
   };
@@ -104,16 +111,17 @@ const UserProfile = () => {
 
   const handleConfirmDelete = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       await axios.delete(`/api/users/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
         withCredentials: true,
       });
-      navigate('/users'); // Redirect to the users list page after deletion
+      navigate("/users");
+      toast.success("User deleted successfully!");
     } catch (err) {
-      console.error('Error deleting user:', err);
+      console.error("Error deleting user:", err);
       setError(err.message);
     }
   };
@@ -122,17 +130,36 @@ const UserProfile = () => {
     setIsDeleteModalOpen(false);
   };
 
-  const handleChange = (e) => {
+  const handleChange = async (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
+
+    if (name === "email") {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          `/api/users/check-email-update?email=${value}&userId=${formData.userId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+          }
+        );
+        setEmailUnique(response.data.isUnique);
+        setEmailError(response.data.isUnique ? "" : "Email already taken");
+      } catch (error) {
+        console.error("Error checking email uniqueness", error);
+      }
+    }
   };
 
   const handleRequestUpdate = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       const response = await axios.get(`/api/users/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -142,13 +169,13 @@ const UserProfile = () => {
       setUser(response.data);
       setPendingRequests(response.data.pendingRequests);
     } catch (err) {
-      console.error('Error fetching updated user data:', err);
+      console.error("Error fetching updated user data:", err);
     }
   };
 
   const handleAccessUpdate = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       const response = await axios.get(`/api/users/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -157,7 +184,7 @@ const UserProfile = () => {
       });
       setUser(response.data);
     } catch (err) {
-      console.error('Error fetching updated user data:', err);
+      console.error("Error fetching updated user data:", err);
     }
   };
 
@@ -171,122 +198,165 @@ const UserProfile = () => {
         <Header />
 
         <div className="p-6 space-y-4">
-        
-        <h2 className="text-xl font-semibold text-slate-800 dark:text-slate-200 mb-4">
+          <h2 className="text-xl font-semibold text-slate-800 dark:text-slate-200 mb-4">
             User Profile
           </h2>
 
-        <div className="p-4 border dark:border-none rounded-lg shadow-sm bg-white dark:bg-slate-600">
-          <div className="flex items-center justify-between">
-            {/* User Profile and Details */}
-            <div className="flex items-center">
-              <img
-                src={user.profilePicture || avatar} 
-                alt="User Avatar"
-                className="w-32 h-32 object-cover rounded-full"
-              />
-              <div className="ml-4">
-                <h2 className="text-2xl font-bold text-slate-700 dark:text-slate-100 mb-3"> {user.firstName} {user.lastName} </h2>
-                <p className="text-slate-700 dark:text-slate-300 mb-2"><strong>User ID:</strong> {user.userId}</p>
-                <p className="text-slate-700 dark:text-slate-300"><strong>Email:</strong> {user.email}</p>
+          <div className="p-4 border dark:border-none rounded-lg shadow-sm bg-white dark:bg-slate-600">
+            <div className="flex items-center justify-between">
+              {/* User Profile and Details */}
+              <div className="flex items-center">
+                <img
+                  src={user.profilePicture || avatar}
+                  alt="User Avatar"
+                  className="w-32 h-32 object-cover rounded-full"
+                />
+                <div className="ml-4">
+                  <h2 className="text-2xl font-bold text-slate-700 dark:text-slate-100 mb-3">
+                    {" "}
+                    {user.firstName} {user.lastName}{" "}
+                  </h2>
+                  <p className="text-slate-700 dark:text-slate-300 mb-2">
+                    <strong>User ID:</strong> {user.userId}
+                  </p>
+                  <p className="text-slate-700 dark:text-slate-300">
+                    <strong>Email:</strong> {user.email}
+                  </p>
+                </div>
+              </div>
+
+              {/* Action Buttons */}
+              <div className="flex flex-col gap-2">
+                <button
+                  onClick={handleEdit}
+                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                >
+                  Edit User
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+                >
+                  Delete User
+                </button>
               </div>
             </div>
+          </div>
 
-            {/* Action Buttons */}
-            <div className="flex flex-col gap-2">
+          {/* Pending Door Permission Requests */}
+          <UPPermissionRequests
+            pendingRequests={pendingRequests}
+            onRequestUpdate={handleRequestUpdate}
+          />
+
+          {/* Door Access Table */}
+          <UPDoorAccess
+            accessRecords={user.doorAccess || []}
+            userId={user._id}
+            onAccessUpdate={handleAccessUpdate}
+          />
+
+          {/* Door Access History */}
+          <UPHistory historyRecords={historyRecords} />
+
+          {/* Edit User Modal */}
+          <Modal isVisible={isEditModalOpen} onClose={handleCloseEditModal}>
+            <h2 className="text-xl text-slate-700 dark:text-slate-200 font-bold mb-4">
+              Edit User
+            </h2>
+            <div className="mb-4">
+              <label className="block text-slate-700 dark:text-slate-200">
+                User ID
+              </label>
+              <input
+                type="text"
+                name="userId"
+                value={formData.userId}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+                disabled // Disable input for User ID
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-slate-700 dark:text-slate-200">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-slate-700 dark:text-slate-200">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+              />
+            </div>
+            <div className="mb-4 relative">
+              <label className="block text-slate-700 dark:text-slate-200">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
+              />
+              {emailUnique !== null && (
+                <span className="absolute right-3 top-10 transform -translate-y-1/2 text-lg">
+                  {emailUnique ? (
+                    <FaCheckCircle className="text-green-500" />
+                  ) : (
+                    <FaTimesCircle className="text-red-500" />
+                  )}
+                </span>
+              )}
+              {emailError && (
+                <p className="text-red-500 mt-1 flex items-center">
+                  <BiError className="mr-1" /> {emailError}
+                </p>
+              )}
+            </div>
+            <div className="flex justify-end gap-2">
               <button
-                onClick={handleEdit}
-                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                onClick={handleCloseEditModal}
+                className="bg-gray-500 w-20 dark:bg-slate-500 text-white px-4 py-2 rounded"
               >
-                Edit User
+                Cancel
               </button>
               <button
-                onClick={handleDelete}
-                className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+                onClick={handleSave}
+                className={`w-20 px-4 py-2 rounded ${
+                  !emailUnique
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-blue-500 text-white"
+                }`}
+                disabled={!emailUnique} // Disable submit if email is not unique
               >
-                Delete User
+                Save
               </button>
             </div>
-          </div>
-        </div>
+          </Modal>
 
-        {/* Pending Door Permission Requests */}
-        <UPPermissionRequests pendingRequests={pendingRequests} onRequestUpdate={handleRequestUpdate} />
-
-        {/* Door Access Table */}
-        <UPDoorAccess accessRecords={user.doorAccess || []} userId={user._id} onAccessUpdate={handleAccessUpdate} />
-
-        {/* Door Access History */}
-        <UPHistory historyRecords={historyRecords} />
-
-        {/* Edit User Modal */}
-        <Modal isVisible={isEditModalOpen} onClose={handleCloseEditModal}>
-          <h2 className="text-xl text-slate-700 dark:text-slate-200 font-bold mb-4">Edit User</h2>
-          <div className="mb-4">
-            <label className="block text-slate-700 dark:text-slate-200">First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-slate-700 dark:text-slate-200">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-slate-700 dark:text-slate-200">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-slate-700 dark:text-slate-200">User ID</label>
-            <input
-              type="text"
-              name="userId"
-              value={formData.userId}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2  dark:bg-slate-600 dark:text-slate-100 focus:ring-blue-400"
-            />
-          </div>
-          <div className="flex justify-end gap-2">
-            <button
-              onClick={handleCloseEditModal}
-              className="bg-gray-500 w-20 dark:bg-slate-500 text-white px-4 py-2 rounded"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={handleSave}
-              className="bg-blue-500 w-20 text-white px-4 py-2 rounded"
-            >
-              Save
-            </button>
-          </div>
-        </Modal>
-
-        {/* Delete User Modal */}
-        <ConfirmationModal
-          isOpen={isDeleteModalOpen}
-          onClose={handleCloseDeleteModal}
-          onConfirm={handleConfirmDelete}
-          message="Are you sure you want to delete this user? This action cannot be undone."
-        />
+          {/* Delete User Modal */}
+          <ConfirmationModal
+            isOpen={isDeleteModalOpen}
+            onClose={handleCloseDeleteModal}
+            onConfirm={handleConfirmDelete}
+            message="Are you sure you want to delete this user? This action cannot be undone."
+          />
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
