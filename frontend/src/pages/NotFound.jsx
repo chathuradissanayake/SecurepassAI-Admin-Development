bb689a52199d653c042b1aff342325bee08b946c
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-01-14 16:19:58 +0530

    Enhance the UI of Rejected request in User Profile and rename the component

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -5,11 +5,11 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-lg mb-4">The page you are looking for does not exist.</p>
+    <div className="flex flex-col items-center justify-center h-screen dark:bg-slate-700">
+      <h1 className="text-4xl font-bold mb-4 dark:text-slate-300">404 - Page Not Found</h1>
+      <p className="text-lg dark:text-slate-300 mb-4">The page you are looking for does not exist.</p>
       <button
-        onClick={() => navigate('/')}
+        onClick={() => navigate('/dashboard')}
         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
       >
         Go to Home
